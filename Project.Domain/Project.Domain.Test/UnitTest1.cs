685d99b6fa647a500c1e53d6917b13d4a5adaff1
uberacyjr <uberacyjr@outlook.com>
2016-09-18 20:52:19 -0300

    Performance Test and generic + lambda expression methods

---
+++ Project.Domain/Project.Domain.Test/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using Project.Domain.Test.BD;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace Project.Domain.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private MyDbContext _context;
+        [TestInitialize]
+        public void SetUp()
+        {
+            _context = new MyDbContext();
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a =   _context.Ceps.GetWhereNoTracking(x => x.Id == 1).ToList();
+
+            var c = _context.Ceps
+                            .GetWhereByCompleteDate(x => x.Id == 1, x => new Retorno {Id =x.Id, Nome= x.Logradouro })
+                            .ToList();
+
+        }
+    }
+    public class Retorno
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+    }
+    public static class Helper
+    {
+
+        public static IEnumerable<T> GetWhereNoTracking<T>(this IQueryable<T> source, Expression<Func<T,bool>> predicate) where T : class
+        {
+            return source.AsNoTracking().Where(predicate);
+        }
+        public static IEnumerable<T> GetAllNoTracking<T>(this IQueryable<T> source) where T : class
+        {
+            return source.AsNoTracking();
+        }
+
+        public static IEnumerable<Retorno> GetWhereByCompleteDate<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate, Expression<Func<T, Retorno>> proje) where T : class
+        {
+            return  source.Where(predicate)
+                          .Select(proje);
+        }
+    }
+}
